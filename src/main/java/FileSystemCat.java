6d4a9b63f46458b3eccf3b8d2f3fbc13e23ac727
asker124143222 <124143222@qq.com>
2019-08-18 22:38:41 +0800

    update

--- src/main/java/FileSystemCat.java
+++ src/main/java/FileSystemCat.java
@@ -1,4 +1,5 @@
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
+//import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.*;
 import org.apache.hadoop.io.IOUtils;
@@ -20,7 +21,7 @@ import java.time.ZoneId;
  */
 public class FileSystemCat {
 
-    private static String HDFSUri = "hdfs://bigdata-senior01.home.com:9000";
+    private static String HDFSUri = "hdfs://vmhome10.com:9000";
 
     public static void main(String[] args) throws Exception {
         long startTime = System.currentTimeMillis();
@@ -34,12 +35,16 @@ public class FileSystemCat {
 //        copyFileToLocal(args);
 
         //文件写入
-  //      writeFile(args);
+//        writeFile(args);
 
-        readFile(args);
+        //读取远程地址，写入本地
+//        readFile(args);
 
         //file status
-        fileStatus(args);
+//        fileStatus(args);
+
+        //查看文件名称、权限、长度、块信息
+        fileStatus2(args);
 
         // file status pattern
 //        filePattern(args);
@@ -81,7 +86,7 @@ public class FileSystemCat {
         String uri = args[0];
         Configuration conf = new Configuration();
 //        conf.set("fs.hdfs.impl",org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
-        conf.set("fs.defaultFS","hdfs://bigdata-senior01.home.com:9000");
+        conf.set("fs.defaultFS",HDFSUri);
         FileSystem fs = FileSystem.get(URI.create(uri), conf);
 //        InputStream in  = null;
 //        FSDataInputStream继承 java.io.DataInputStream,支持随机访问
@@ -89,6 +94,8 @@ public class FileSystemCat {
         try {
             in = fs.open(new Path(uri));
             IOUtils.copyBytes(in, System.out, 4096, false);
+
+            //重新定位到文件头，在cat一次
             in.seek(0);
             IOUtils.copyBytes(in, System.out, 4096, false);
         } finally {
@@ -107,7 +114,7 @@ public class FileSystemCat {
         InputStream in = new BufferedInputStream(new FileInputStream(locaSrc));
         //从windows环境提交的时候需要设置hadoop用户名
         //在linux的其他用户环境下估计也需要
-        System.setProperty("HADOOP_USER_NAME","hadoop");
+//        System.setProperty("HADOOP_USER_NAME","hadoop");
 
         Configuration conf = new Configuration();
 //        conf.set("fs.DefaultFs", "hdfs://bigdata-senior01.home.com:9000");
@@ -116,7 +123,8 @@ public class FileSystemCat {
           //如果不涉及两个文件系统，dst写短路径是没问题的
 //        FileSystem fs = FileSystem.get(URI.create(dst), conf);
 
-        FileSystem fs = FileSystem.get(URI.create(HDFSUri),conf);
+        //这里如果没有指定用户，就需要使用  System.setProperty("HADOOP_USER_NAME","hadoop");
+        FileSystem fs = FileSystem.get(URI.create(HDFSUri),conf,"hadoop");
 
 //        fs.copyFromLocalFile(new Path(locaSrc),new Path(dst));
 //        fs.close();
@@ -130,6 +138,8 @@ public class FileSystemCat {
         out = fs.create(new Path(dst), () -> System.out.print("."));
 
         IOUtils.copyBytes(in, out, 4096, true);
+        IOUtils.closeStream(in);
+        IOUtils.closeStream(out);
         fs.close();
     }
 
@@ -140,16 +150,55 @@ public class FileSystemCat {
 
         Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(URI.create(HDFSUri),conf);
+
+        fs.copyToLocalFile(new Path(remoteSrc),new Path(localDest));
+
         FSDataInputStream in = fs.open(new Path(remoteSrc));
         OutputStream out = new BufferedOutputStream(new FileOutputStream(localDest));
         IOUtils.copyBytes(in,out,4096,true);
+        IOUtils.closeStream(in);
+        IOUtils.closeStream(out);
         fs.close();
+    }
+
+    //查看文件名称、权限、长度、块信息
+    private static void fileStatus2(String[] args) throws Exception{
+        Configuration conf = new Configuration();
+//        conf.set("fs.defaultFS",HDFSUri);
+
+        Path file = new Path(args[0]);
+
+        try (FileSystem fs = FileSystem.get(new URI(HDFSUri),conf,"hadoop")) {
+            RemoteIterator<LocatedFileStatus> listFiles = fs.listFiles(file, true);
+            while (listFiles.hasNext()){
+                LocatedFileStatus status = listFiles.next();
+                System.out.println("name :"+status.getPath().getName());
+                System.out.println("path: "+status.getPath());
+                System.out.println("length: "+status.getLen());
+                System.out.println("group: "+ status.getGroup());
+                System.out.println("permission: "+status.getPermission());
+                BlockLocation[] blockLocations = status.getBlockLocations();
+                for(BlockLocation location : blockLocations){
+                    long length = location.getLength();
+                    long offset = location.getOffset();
+                    StorageType[] storageTypes = location.getStorageTypes();
+                    String[] storageIds = location.getStorageIds();
+                    String[] topologyPaths = location.getTopologyPaths();
+                    String[] names = location.getNames();
+                    String[] hosts = location.getHosts();
+                    for(int i=0;i<hosts.length;i++){
+                        System.out.println("length: "+length+"  offset: "+offset+"  host: "+hosts[i]);
+                    }
+                }
+                System.out.println("---------------------------------");
+            }
+        }
     }
 
     //查找文件，递归列出给定目录或者文件的属性
     private static void fileStatus(String[] args) throws IOException {
         Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", "hdfs://bigdata-senior01.home.com:9000");
+        conf.set("fs.defaultFS", HDFSUri);
         Path file = new Path(args[0]);
         FileSystem fs = null;
         try {
@@ -171,10 +220,12 @@ public class FileSystemCat {
             for (FileStatus fileStatus : fileStatuses) {
                 System.out.println("-------------->");
                 System.out.println("是否目录：" + fileStatus.isDirectory());
-                System.out.println("path:" + fileStatus.getPath().toString());
-                System.out.println("length:" + fileStatus.getLen());
-                System.out.println("accessTime:" + LocalDateTime.ofInstant(Instant.ofEpochMilli(fileStatus.getAccessTime()), ZoneId.systemDefault()));
-                System.out.println("permission:" + fileStatus.getPermission().toString());
+                System.out.println("path: " + fileStatus.getPath().toString());
+                System.out.println("name: " + fileStatus.getPath().getName());
+                System.out.println("length: " + fileStatus.getLen());
+                System.out.println("accessTime: " + LocalDateTime.ofInstant(Instant.ofEpochMilli(fileStatus.getAccessTime()), ZoneId.systemDefault()));
+                System.out.println("permission: " + fileStatus.getPermission().toString());
+                System.out.println("replication: "+fileStatus.getReplication());
                 //递归查找子目录
                 if (fileStatus.isDirectory()) {
                     FileSystemCat.fileStatus(new String[]{fileStatus.getPath().toString()});
@@ -189,7 +240,7 @@ public class FileSystemCat {
     //查找文件，通配模式，不能直接用于递归，应该作为递归的最外层，不进入递归
     private static void filePattern(String[] args) throws IOException {
         Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", "hdfs://bigdata-senior01.home.com:9000");
+        conf.set("fs.defaultFS", HDFSUri);
         FileSystem fs = null;
         try {
             fs = FileSystem.get(conf);
@@ -221,12 +272,15 @@ public class FileSystemCat {
         }
     }
 
-    private static void writeFile(String[] args) throws IOException{
+    //往远程服务器上写一个文件
+    private static void writeFile(String[] args) throws Exception{
         Path f = new Path(args[0]);
         Text inputString = new Text(args[1]);
         Configuration conf = new Configuration();
-        conf.set("fs.defaultFS",HDFSUri);
-        FileSystem fs = FileSystem.get(conf);
+        //只保存两个副本
+        conf.set("dfs.replication","2");
+//        conf.set("fs.defaultFS",HDFSUri);
+        FileSystem fs = FileSystem.get(new URI(HDFSUri), conf,"hadoop");
         FSDataOutputStream out =  fs.create(f,true);
         byte[] bytes = serialize(inputString);
         out.write(bytes);
@@ -234,21 +288,31 @@ public class FileSystemCat {
         fs.close();
     }
 
+    //远程文件读取，写入本地
     private static void readFile(String[] args) throws IOException{
-        Path f = new Path(args[0]);
+        Path remoteSource = new Path(args[0]);
+        String localDest = args[1];
+
         Configuration conf = new Configuration();
         conf.set("fs.defaultFS",HDFSUri);
         FileSystem fs = FileSystem.get(conf);
-        FSDataInputStream in = fs.open(f);
+        FSDataInputStream in = fs.open(remoteSource);
+        OutputStream out = new BufferedOutputStream(new FileOutputStream(localDest));
         Writable writable = new Text();
         byte[] bytes = new byte[1024];
+        for(int bytesRead=in.read(bytes);bytesRead>0;bytesRead=in.read(bytes)){
+            System.out.println(new String(bytes));
+            out.write(bytes,0,bytesRead);
 
-        int length = in.read(bytes,0,1024);
-        System.out.println("length: "+length+",byte[]: "+new String(bytes));
-        bytes = derialize(writable,bytes);
-        System.out.println("writable:"+writable.toString()+",byte[]: "+new String(bytes));
+        }
+
+//        int length = in.read(bytes,0,4096);
+//        System.out.println("length: "+length+",byte[]: \n"+new String(bytes));
+//        bytes = derialize(writable,bytes);
+//        System.out.println("writable:\n"+writable.toString()+"\nbyte[]: \n"+new String(bytes));
         in.close();
         fs.close();
+        out.close();
 
     }
 
@@ -262,6 +326,7 @@ public class FileSystemCat {
 
     private static byte[] derialize(Writable writable,byte[] bytes) throws IOException{
         ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+
         DataInputStream dataIn = new DataInputStream(in);
         writable.readFields(dataIn);
         dataIn.close();
